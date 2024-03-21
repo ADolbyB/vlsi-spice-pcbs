9a03f8c383fc7e89a4c2e7f4c13a546487925589
Joel Brigida <joel@drtechreps.com>
2024-03-20 21:26:11 -0400

    Fixed 1-bit Full Adder Simulation

--- EDAPlayground/Full-Adder1/Files/testbench.sv
+++ EDAPlayground/Full-Adder1/Files/testbench.sv
@@ -3,11 +3,11 @@
 module tb_FULLADD_1;
 
     localparam CLK_PER = 10; // Clock period
-    reg clk = 0; 			 // Initialize Clock signal to Zero
-  	reg X, Y;    			 // Inputs
+    reg clk = 0; 			       // Initialize Clock signal to Zero
+  	reg X, Y;    			       // Inputs
     reg CINtest;
     
-    wire COUTtest;			 // Outputs
+    wire COUTtest;			     // Outputs
   	wire SUMtest;
 
   	// Clock Generation
@@ -15,16 +15,16 @@ module tb_FULLADD_1;
 
     // Connect Test Bench to the Module Under Test
   	fulladd1 fa1 (
-      .A(X),
-      .B(Y), 
-      .CIN(CINtest),
-      .COUT(COUTtest),
-      .SUM(SUMtest)
+        .A(X),
+        .B(Y), 
+        .CIN(CINtest),
+        .COUT(COUTtest),
+        .SUM(SUMtest)
     );
 
     // Test Routine Start:
     initial begin
-    	X = 1'b0; // Change values as needed
+        X = 1'b0;             // Change values as needed
         Y = 1'b0;
         CINtest = 0;
 
@@ -37,14 +37,14 @@ module tb_FULLADD_1;
       	#10 X = 1'b1; Y = 1'b0; CINtest = 1;
       	#10 X = 1'b1; Y = 1'b1; CINtest = 0;
         #10 X = 1'b1; Y = 1'b1; CINtest = 1;
-      	#40 $finish;
+      	#10 $finish;
     end
 
     always @(posedge clk) begin
-    	$display("Time = %0t \tX = %b \tY = %b \tCIN = %b \tSUM = %b \tCOUT = %b", $time,X,Y,CINtest,SUMtest,COUTtest);
+    	  $display("Time = %0t \tX = %b \tY = %b \tCIN = %b \tSUM = %b \tCOUT = %b", $time,X,Y,CINtest,SUMtest,COUTtest);
     end
   	initial begin
       	$dumpfile("tb_FULLADD_1.vcd"); 	// Extract waveforms from test bench to file.
-      	$dumpvars(0, tb_FULLADD_1);		// Extract Variable values to file.
+      	$dumpvars(0, tb_FULLADD_1);		  // Extract Variable values to file.
   	end
 endmodule
